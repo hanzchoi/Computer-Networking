db661c60581f1b64cea887e6cd879d66d75e2621
personabe1984 <personabe1984@gmail.com>
2015-12-06 14:20:49 -0500

    first commit

---
+++ src/nyc/c4q/personabe1984/Main.java
@@ -0,0 +1,10 @@
+package nyc.c4q.personabe1984;
+
+public class Main {
+
+    //Create a server that accepts a UTC
+    // offset and returns the time in ISO 8601 format. Write a client that prompts the user to test.
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
